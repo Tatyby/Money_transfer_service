d458d6ec2a2de65b37126a296c2ade17c1cd77a8
Tatiana <in_sdo@mail.ru>
2023-07-17 10:11:06 +0300

    add package config

---
+++ src/main/java/com/example/money_transfer_service/config/Config.java
@@ -0,0 +1,17 @@
+package com.example.money_transfer_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:5500/transfer");
+            }
+        };
+    }
+}
